b9e9c11979437b47cd4cbedc2618207b0addba9c
chrisb3546 <chrisbradycode@gmail.com>
2020-12-17 14:58:58 -0500

    attempt to fix oauth by fixing variable names in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV['google_client_key'], ENV['google_client_secret']
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
   end

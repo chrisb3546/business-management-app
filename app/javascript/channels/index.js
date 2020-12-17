ea419f9c35ca24efb741a66b4f7e234483acc81c
chrisb3546 <chrisbradycode@gmail.com>
2020-12-17 14:52:23 -0500

    remove oauth.io

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,13 +3,5 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
-OAuth.initialize('<%= ENV["OAUTHIO_PUBLIC_KEY"] %>');
-OAuth.popup('google')
-    .done(function(result) {
-      //use result.access_token in your API request
-      //or use result.get|post|put|del|patch|me methods (see below)
-    })
-    .fail(function (err) {
-      //handle error with err
-});
+
 

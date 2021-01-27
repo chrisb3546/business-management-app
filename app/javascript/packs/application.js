8794f30992eb9c029060a60c7c40032c864c3ce5
chrisb3546 <chrisbradycode@gmail.com>
2021-01-27 15:39:44 -0500

    add bootstrap to rails app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,13 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import '../../assets/stylesheets/style' 
+// import '../../assets/stylesheets/style' 
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
 
 
 
@@ -17,3 +19,4 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "bootstrap"
